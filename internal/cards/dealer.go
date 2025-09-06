787083303719a7f97670c41cf5af493b0c65f789
hartun9 <h4rtun9@gmail.com>
2025-09-06 15:50:31 +0900

    refactor: 固定配列をsliceに変更

--- internal/cards/dealer.go
+++ internal/cards/dealer.go
@@ -3,15 +3,15 @@ package cards
 type Dealer struct{}
 
 // StartGame 参加人数を受け取り、それぞれのプレイヤーにカードを配る
-func (Dealer) StartGame(table Table) [][2]*Card {
+func (Dealer) StartGame(table Table) [][]*Card {
 	deck := NewDeck()
 	deck.ShuffleDeck()
-	var playerCards [][2]*Card
+	var playerCards [][]*Card
 	for i := 0; i < table.AmountOfPlayers; i++ {
-		var playerHand [2]*Card
+		var playerHand []*Card
 		for j := 0; j < 2; j++ {
 			card1 := deck.Draw()
-			playerHand[j] = card1
+			playerHand = append(playerHand, card1)
 		}
 		playerCards = append(playerCards, playerHand)
 	}
