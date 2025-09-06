017842770e4d7d8f8a6f02ac77442dd1575d3482
hartun9 <h4rtun9@gmail.com>
2025-09-06 15:32:52 +0900

    refactor: rename to game1

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	dealer := cards.Dealer{}
-	table1 := dealer.StartGame(4)
-	for _, card := range table1[0] {
+	game1 := dealer.StartGame(4)
+	for _, card := range game1[0] {
 		fmt.Println(card.GetCardString())
 	}
 }
