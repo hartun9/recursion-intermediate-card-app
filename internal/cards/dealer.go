b03765cbcd1f798b4086f795d575de7e8b1919ca
hartun9 <h4rtun9@gmail.com>
2025-09-06 15:53:13 +0900

    refactor: extract InitialCards

--- internal/cards/dealer.go
+++ internal/cards/dealer.go
@@ -9,11 +9,20 @@ func (Dealer) StartGame(table Table) [][]*Card {
 	var playerCards [][]*Card
 	for i := 0; i < table.AmountOfPlayers; i++ {
 		var playerHand []*Card
-		for j := 0; j < 2; j++ {
+		dealer := Dealer{}
+		for j := 0; j < dealer.InitialCards(table.GameMode); j++ {
 			card1 := deck.Draw()
 			playerHand = append(playerHand, card1)
 		}
 		playerCards = append(playerCards, playerHand)
 	}
 	return playerCards
+}
+
+func (Dealer) InitialCards(gameMode string) int {
+	switch gameMode {
+	case "21":
+		return 2
+	}
+	return 0
 }
--- internal/cards/table.go
+++ internal/cards/table.go
@@ -2,4 +2,5 @@ package cards
 
 type Table struct {
 	AmountOfPlayers int
+	GameMode        string
 }
