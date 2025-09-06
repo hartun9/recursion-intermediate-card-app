e1a7094d8863db09a1635ab13085c2f49cf37060
hartun9 <h4rtun9@gmail.com>
2025-09-06 18:54:11 +0900

    feat: add "21" case

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 func main() {
 	dealer := cards.Dealer{}
 
+	table1 := cards.Table{AmountOfPlayers: 3, GameMode: "21"}
+	game1 := dealer.StartGame(table1)
+	dealer.PrintTableInformation(game1, table1)
+
 	table2 := cards.Table{AmountOfPlayers: 4, GameMode: "poker"}
 	game2 := dealer.StartGame(table2)
 	dealer.PrintTableInformation(game2, table2)
