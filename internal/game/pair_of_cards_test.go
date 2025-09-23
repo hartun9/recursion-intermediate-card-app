83488d272602e6c5b58b5b36757cbd8d3001bd08
hartun9 <h4rtun9@gmail.com>
2025-09-23 21:10:05 +0900

    test: add ペアの枚数が同じケース

--- internal/game/pair_of_cards_test.go
+++ internal/game/pair_of_cards_test.go
@@ -19,6 +19,12 @@ func TestWinnerPairOfCards(t *testing.T) {
 			p2:   []int{4, 11, 11, 12, 3},
 			want: "player 1 is the winner",
 		},
+		{
+			name: "ペアの枚数が同じで、player 2 の rank が高い",
+			p1:   []int{4, 8, 8, 4, 11},
+			p2:   []int{4, 11, 11, 12, 3},
+			want: "player 2 is the winner",
+		},
 	}
 
 	for _, tt := range tests {
